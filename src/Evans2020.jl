36f9da5dcdf91bb94248e8ad97bfd90790fcc6c5
popohacker <61708167+popohacker@users.noreply.github.com>
2021-05-21 13:24:44 +0200

    Update Evans2020.jl

--- src/Evans2020.jl
+++ src/Evans2020.jl
@@ -14,7 +14,109 @@ using Optim
 using LeastSquaresOptim 
 
 
+"""
+trunc_norm_draws(unif_vals::Any, mu::Any, sigma::Any, cut_lb::Any = nothing, cut_ub::Any = nothing)
+    if cut_lb == nothing && cut_ub == nothing
 
+    --------------------------------------------------------------------
+    Draw (N x S) matrix of random draws from a truncated normal
+    distribution based on a normal distribution with mean mu and
+    standard deviation sigma and cutoffs (cut_lb, cut_ub). These draws
+    correspond to an (N x S) matrix of randomly generated draws from a
+    uniform distribution U(0,1).
+    --------------------------------------------------------------------
+    INPUTS:
+    unif_vals = (N, S) matrix, (N,) vector, or scalar in (0,1), random
+                draws from uniform U(0,1) distribution
+    mu        = scalar, mean of the nontruncated normal distribution
+                from which the truncated normal is derived
+    sigma     = scalar > 0, standard deviation of the nontruncated
+                normal distribution from which the truncated normal is
+                derived
+    cut_lb    = scalar or string, ='None' if no lower bound cutoff is
+                given, otherwise is scalar lower bound value of
+                distribution. Values below this cutoff have zero
+                probability
+    cut_ub    = scalar or string, ='None' if no upper bound cutoff is
+                given, otherwise is scalar lower bound value of
+                distribution. Values below this cutoff have zero
+                probability
+
+    OTHER FUNCTIONS AND FILES CALLED BY THIS FUNCTION:
+        scipy.stats.norm()
+
+    OBJECTS CREATED WITHIN FUNCTION:
+    cut_ub_cdf  = scalar in [0, 1], cdf of N(mu, sigma) at upper bound
+                  cutoff of truncated normal distribution
+    cut_lb_cdf  = scalar in [0, 1], cdf of N(mu, sigma) at lower bound
+                  cutoff of truncated normal distribution
+    unif2_vals  = (N, S) matrix, (N,) vector, or scalar in (0,1),
+                  rescaled uniform derived from original.
+    tnorm_draws = (N, S) matrix, (N,) vector, or scalar in (0,1),
+                  values drawn from truncated normal PDF with base
+                  normal distribution N(mu, sigma) and cutoffs
+                  (cut_lb, cut_ub)
+
+    FILES CREATED BY THIS FUNCTION: None
+
+    RETURNS: tnorm_draws
+    --------------------------------------------------------------------
+
+
+"""
+function trunc_norm_draws(unif_vals::Any, mu::Any, sigma::Any, cut_lb::Any = nothing, cut_ub::Any = nothing)
+    if cut_lb == nothing && cut_ub == nothing
+        cut_ub_cdf = 1.0
+        cut_lb_cdf = 0.0
+    elseif cut_lb != nothing && cut_ub == nothing
+        cut_ub_cdf = 1.0
+        cut_lb_cdf = cdf.(Normal(mu, sigma),  cut_lb)
+    elseif cut_lb == nothing && cut_ub != nothing
+        cut_ub_cdf = cdf.(Normal(mu, sigma), cut_ub)
+        cut_lb_cdf = 0.0
+    elseif cut_lb != nothing && cut_ub != nothing
+        cut_ub_cdf = cdf.(Normal(mu, sigma), cut_ub)
+        cut_lb_cdf = cdf.(Normal(mu, sigma),  cut_lb)
+    end
+    unif2_vals = transpose(reduce(hcat, unif_vals)) .* (cut_ub_cdf - cut_lb_cdf) .+ cut_lb_cdf 
+    tnorm_draws = quantile.(Normal(mu, sigma), unif2_vals)
+    return tnorm_draws
+end
+
+
+"""
+    get_Y(k2t, zt, args)
+
+    --------------------------------------------------------------------
+    Calculate aggregate output
+    --------------------------------------------------------------------
+    INPUTS:
+    Kt   =
+    Lt   =
+    zt   =
+    args =
+
+    RETURNS: Yt
+"""
+
+function get_Y(k2t, zt, args)
+    nvec, epsilon, alpha = args
+    close_tol = 1e-6
+    Kt = k2t
+    Lt = sum(nvec) 
+    At = exp.(zt)
+    if isapprox(epsilon, 1.0, atol=close_tol) == true
+        Yt = At .* ((Kt).^alpha)*((Lt).^(1- alpha))
+    elseif isinf(epsilon) == true
+        Yt = At .* (alpha .* Kt .+ (1 - alpha) .* Lt)
+    elseif epsilon > 0 & isapprox(epsilon, 1.0, atol=close_tol) == false & isinf(epsilon) == false
+        Yt = At .* (alpha.*(Kt.^((epsilon -1)/epsilon)) + (1-alpha) *(Lt.^((epsilon-1)/epsilon))).^(epsilon/(epsilon-1))
+    elseif epsilon <= 0 
+        err_msg = "ERROR get_Y(): epsilon <=0"
+        print(err_msg)
+    end
+return Yt
+end
 
 """
 sim_timepath(
@@ -131,7 +233,9 @@ end
 
 
 
-export sim_timepath
+export      sim_timepath,
+            trunc_norm_draws,
+            get_Y
 
 end
 
